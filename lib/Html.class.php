96abf720064d61a3ceb584b5162dcce5e11e6bd0
Alex Regan <alex.regan@artisancolour.com>
2016-09-04 23:57:46 -0700

    added methods for styles & attrs

--- lib/Html.class.php
+++ lib/Html.class.php
@@ -12,7 +12,7 @@ class Html {
   public static function createElement( $tagName, $content, $attributes = null ) {
     $newAttributes = '';
 
-    if ( !empty($attributes) ) {
+    if ( !empty($attributes) && is_array( $attributes ) ) {
       $mappedAttributes = [];
       foreach ($attributes as $name => $value) {
         $mappedAttributes[] = Html::createAttribute( $name, $value );
@@ -38,7 +38,6 @@ class Html {
   function __construct( $tagName, $content = '' ) {
     $this->tagName = $tagName;
     $this->content = $content;
-
     return $this;
   }
 
@@ -46,27 +45,73 @@ class Html {
     if ( !in_array($className, $this->classNames) ) {
       array_push( $this->classNames, $className );
     }
-
     return $this;
+  }
+
+  public function addClasses( $classList ) {
+    array_map( [$this, 'addClass'], $classList );
+    return $this;
+  }
+
+  private function getClasses() {
+    if ( !empty($this->classNames) ) {
+      return 'class="' . implode( ' ', $this->classNames ) . '"';
+    } else {
+      return;
+    }
   }
 
   public function addAttribute( $name, $value ) {
     array_push( $this->attributes, Html::createAttribute( $name, $value ) );
-
     return $this;
+  }
+
+  public function addAttributes( $attributeList ) {
+    array_map( [$this, 'addAttribute'], $attributeList );
+    return $this;
+  }
+
+  private function getAttributes() {
+    if ( !empty($this->attributes) ) {
+      return implode( ' ', $this->attributes );
+    } else {
+      return;
+    }
+  }
+
+  public function addStyle( $name, $value ) {
+    array_push( $this->styles, "$name:$value" );
+    return $this;
+  }
+
+  public function addStyles( $styleList ) {
+    foreach ( $styleList as $name => $value ) {
+      $this->addStyle( $name, $value );
+    }
+    return $this;
+  }
+
+  public function getStyles() {
+    if ( !empty($this->styles) ) {
+      return 'style="' . implode( ';', $this->styles ) . '"';
+    } else {
+      return;
+    }
   }
 
   public function addText( $text ) {
     $this->content = $text;
-
     return $this;
   }
 
-  public function createChild( $tagName ) {
-    $child = new Html( $tagName );
+  public function createChild( $tagName, $content = '' ) {
+    $child = new Html( $tagName, $content );
     $this->children[] = $child;
-
     return $child;
+  }
+
+  public function renderChildren() {
+    return implode( '', array_map( 'Html::renderNode', $this->children ) );
   }
 
   public function render() {
@@ -74,16 +119,14 @@ class Html {
       '<'
       . implode( ' ', [
           $this->tagName,
-          implode( ' ', [ 'class="', $this->classNames], '"' ),
-          implode( ' ', $this->attributes ),
-          implode( ';', $this->styles ),
+          $this->getClasses(),
+          $this->getAttributes(),
+          $this->getStyles(),
         ])
       . '>'
       . $this->content
-      . implode( '', array_map( 'Html::renderNode', $this->children ) )
-      . '</'
-      . $this->tagName
-      . '>'
+      . $this->renderChildren()
+      . "</{$this->tagName}>"
     );
   }
 
